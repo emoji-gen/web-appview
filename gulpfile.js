187bf2639323e6c174b00e7438018863e20d1c7b
Pine Mizune <pinemz@gmail.com>
2019-07-30 23:48:46 +0900

    feat: add contact page [wip]

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,41 @@
 const del = require('del')
 
 const gulp = require('gulp')
-const ejs = require('gulp-ejs')
+const nunjucks = require('gulp-nunjucks')
 const plumber = require('gulp-plumber')
 const rename = require('gulp-rename')
 
-// --------------------------------------------------------
 
-gulp.task('clean', () =>
-  del(['public/*.html'])
-)
+// ------ ejs ---------------------------------------------
 
 gulp.task('ejs', () =>
-  gulp.src('docs/*.ejs')
-    .pipe(plumber())
-    .pipe(ejs())
+  gulp.src([
+    'docs/**/*.j2',
+    '!docs/*.j2',
+    '!docs/includes/**/*.j2',
+  ])
+    .pipe(nunjucks.compile())
     .pipe(rename({ extname: '.html' }))
     .pipe(gulp.dest('public'))
 )
 
+gulp.task('watch-ejs', () => {
+  gulp.watch(['docs/**/*.j2'], gulp.task('ejs'))
+})
+
+
+// ------ clean -------------------------------------------
+
+gulp.task('clean', () =>
+  del(['public/**/*.html'])
+)
+
+
+// ------ for production ----------------------------------
+
 gulp.task('default', gulp.series('clean', 'ejs'))
 
+
+// ------ for development ---------------------------------
+
+gulp.task('watch', gulp.series('clean', 'ejs', 'watch-ejs'))
