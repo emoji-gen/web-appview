3f855321c79c97981076c0933ee4d599191edbba
Pine Mizune <pinemz@gmail.com>
2019-07-30 22:21:04 +0900

    refactor: remove server side

---
+++ Procfile
@@ -0,0 +1,1 @@
+wp: webpack --watch
---
+++ webpack.config.js
@@ -0,0 +1,138 @@
+'use strict'
+
+const { join } = require('path')
+
+const webpack = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const EventHooksPlugin = require('event-hooks-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+// Detect mode
+//~~~~~~~~~~~~~~~
+const isDev = process.argv.includes('--watch')
+const mode = isDev ? 'development' : 'production'
+
+// Set SASS variables
+//~~~~~~~~~~~~~~~~~~~~~~
+const siteUrl = isDev ? '/appview' : '//emoji-gen.ninja/appview';
+
+module.exports = {
+  mode,
+
+  // Entry and Context
+  //~~~~~~~~~~~~~~~~~~~~
+  context: __dirname,
+  entry: './src/main.js',
+
+
+  // Output
+  //~~~~~~~~~
+  output: {
+    filename: 'script.js',
+    path: join(__dirname, 'dist'),
+  },
+
+
+  // Module
+  //~~~~~~~~~
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { url: false },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { url: false },
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              data: `
+                $site-url: '${siteUrl}';
+              `,
+              includePaths: [ join(__dirname, 'src') ],
+            },
+          },
+        ],
+      },
+    ]
+  },
+
+
+  // Resolve
+  //~~~~~~~~~~
+  resolve: {
+    alias: {
+      '@': __dirname,
+      vue: 'vue/dist/vue.runtime.esm.js',
+    },
+    extensions: ['.js', '.vue', '.yml'],
+  },
+
+
+  // Optimization and Plugins
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({
+        parallel: true,
+      }),
+    ],
+    noEmitOnErrors: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      dry: false,
+      dangerouslyAllowCleanPatternsOutsideProject: true,
+      cleanOnceBeforeBuildPatterns:
+        ['js', 'css'].map(v => join(__dirname, '..', `server/assets/*.${v}`)),
+      verbose: false,
+    }),
+    new EventHooksPlugin({
+      run() { console.log('Mode: ' + mode) },
+      watchRun() { console.log('Mode: ' + mode) },
+    }),
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
+    new webpack.DefinePlugin({ DEBUG: isDev }),
+  ],
+
+
+  // Watch and WatchOptions
+  //~~~~~~~~~~~~~~~~~~~~~~~~~
+  watchOptions: {
+    poll: true,
+    ignored: [ /node_modules/ ],
+  },
+
+
+  // Performance
+  //~~~~~~~~~~~~~~~
+  performance: {
+    hints: false,
+  },
+
+
+  // Stats
+  //~~~~~~~~
+  stats: {
+    entrypoints: true,
+    children: false,
+    colors: true,
+    modules: false,
+  },
+}
